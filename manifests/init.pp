44786eb4d8869c4f3eb0bc348e6076a40b7886cc
root <root@puppet-server.local>
2015-03-08 00:42:21 +0000

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+class hosts (
+	$fqdn_entry	= $::fqdn,
+	$hostname_entry	= $::hostname,
+	)  {
+
+	file { 'hosts_file':
+		path	=> '/etc/hosts',
+		ensure	=> present,
+		owner	=> root,
+		group	=> root,
+		mode	=> 644,
+		content	=> template("hosts/hosts.erb"),
+		before	=> Class[ hosts::entries ],
+		force	=> true,
+		purge	=> true,
+		} 
+
+	include hosts::entries
+
+	} 
+class hosts::entries {
+
+        $myHost = hiera_hash('hosts::hosts_entries')
+	create_resources ( host, $myHost )
+
+}	
