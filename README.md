2620a0b18161cf2155e1fcbd89f355725fadf2f2
root <root@puppet-server.local>
2015-03-09 13:19:05 +0000

    travis fix1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+[![Build Status](https://travis-ci.org/catalinpan/puppet-hosts.svg?branch=master)](https://travis-ci.org/catalinpan/puppet-hosts)
+
 # hosts
 
 #### Table of Contents
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,28 @@
+# ==Class: hosts
+#
+#
+# Manage hosts entries using puppet
+#
+#
 class hosts (
-	$fqdn_entry		= $::fqdn,
-	$hostname_entry		= $::hostname,
-	$localhost_entries	= [],
-	)  {
-
-	file { 'hosts_file':
-		path	=> '/etc/hosts',
-		ensure	=> present,
-		owner	=> root,
-		group	=> root,
-		mode	=> 644,
-		content	=> template("hosts/hosts.erb"),
-		before	=> Class[ hosts::entries ],
-		force	=> true,
-		purge	=> true,
-		} 
-
-	include hosts::entries
-
-	} 
+  $fqdn_entry = $::fqdn,
+  $hostname_entry = $::hostname,
+  $localhost_entries = [],
+  ) {
+  file { 'hosts_file':
+    path => '/etc/hosts',
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => '0644',
+    content => template("hosts/hosts.erb"),
+    before => Class[ hosts::entries ],
+    force => true,
+    purge => true,
+   } 
+include hosts::entries
+} 
 class hosts::entries {
-
-        $myHost = hiera_hash('hosts::hosts_entries')
-	create_resources ( host, $myHost )
-
+  $myHost = hiera_hash('hosts::hosts_entries')
+  create_resources ( host, $myHost )
 }	
