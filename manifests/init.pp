ed6d6404b088ec6121eadab48c4e9e8096a827c4
root <root@puppet-server.local>
2015-03-09 13:24:52 +0000

    travis fix3

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,15 +11,14 @@ class hosts (
   $localhost_entries = [],
   ) {
   file { 'hosts_file':
-    path	=> '/etc/hosts',
-#    ensure	=> present,
-    owner	=> root,
-    group	=> root,
-    mode	=> '0644',
-    content	=> template("hosts/hosts.erb"),
-    before	=> Class[ hosts::entries ],
-    force	=> true,
-    purge	=> true,
+    path     => '/etc/hosts',
+    owner    => root,
+    group    => root,
+    mode     => '0644',
+    content  => template("hosts/hosts.erb"),
+    before   => Class[ hosts::entries ],
+    force    => true,
+    purge    => true,
  }
 include hosts::entries
 } 
